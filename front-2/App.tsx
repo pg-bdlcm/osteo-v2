aa9c4a462d9ccf23446f849a1673f69c9062bef0
Pierre <gordepierre@gmail.com>
2022-08-24 15:43:23 +0200

    DELETE > front folder

---
+++ front-2/App.tsx
@@ -0,0 +1,44 @@
+export { default } from './storybook';
+// import { Button, StyleSheet, TextInput, View, Text } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import styled from 'styled-components/native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.appContainer}>
+//       <View style={styles.inputContainer}>
+//         <TextInput style={styles.textInput} placeholder='placeholder' />
+//         <Button title="Add Goal" />
+//       </View>
+//       <ListContainer>
+//         <Text>List of goals</Text>
+//       </ListContainer>
+//       <StatusBar style="dark" />
+//     </View>
+//   );
+// }
+
+// const ListContainer = styled.View`
+//   border-top-width: 1px;
+//   border-color: #cccccc;
+//   margin-top: 16px;
+//   padding-top: 16px;
+// `
+
+// const styles = StyleSheet.create({
+//   appContainer: {
+//     paddingTop: 50,
+//     paddingHorizontal: 16
+//   },
+//   inputContainer: {
+//     flexDirection: "row",
+//     justifyContent: "space-between"
+//   },
+//   textInput: {
+//     borderWidth: 1,
+//     borderColor: "#cccccc",
+//     paddingLeft: 16,
+//     marginRight: 8,
+//     width: '70%',
+//   },
+// });
---
+++ front-2/storybook/storyLoader.js
@@ -0,0 +1,15 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require("../src/Button.stories");
+}
+
+const stories = ["../src/Button.stories"];
+
+module.exports = {
+  loadStories,
+  stories,
+};
